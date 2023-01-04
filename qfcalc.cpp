1c5c3c37484fc1f5f35880f62baed6c013836ddd
Jade <jadedevofficial@gmail.com>
2023-01-04 20:57:48 +0000

    rm namespace

--- qfcalc.cpp
+++ qfcalc.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
-
 int main() {
     double a, b, c;
 
-    cout << "Enter the value of a: ";
-    cin >> a;
-    cout << "Enter the value of b: ";
-    cin >> b;
-    cout << "Enter the value of c: ";
-    cin >> c;
+    std::cout << "Enter the value of a: ";
+    std::cin >> a;
+    std::cout << "Enter the value of b: ";
+    std::cin >> b;
+    std::cout << "Enter the value of c: ";
+    std::cin >> c;
 
     double root1, root2;
     double discriminant = b*b - 4*a*c;
@@ -19,16 +17,16 @@ int main() {
         // Real roots
         root1 = (-b + sqrt(discriminant)) / (2*a);
         root2 = (-b - sqrt(discriminant)) / (2*a);
-        cout << "The roots are " << root1 << " and " << root2 << endl;
+        std::cout << "The roots are " << root1 << " and " << root2 << std::endl;
     } else if (discriminant == 0) {
         // One real root
         root1 = -b / (2*a);
-        cout << "The root is " << root1 << endl;
+        std::cout << "The root is " << root1 << std::endl;
     } else {
         // Complex roots
         double real = -b / (2*a);
         double imag = sqrt(-discriminant) / (2*a);
-        cout << "The roots are " << real << " + " << imag << "i and " << real << " - " << imag << "i" << endl;
+        std::cout << "The roots are " << real << " + " << imag << "i and " << real << " - " << imag << "i" << std::endl;
     }
 
     return 0;
